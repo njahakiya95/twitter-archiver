cbc0014ecef2a07794eb506cd5b35edc44df131b
Nirmal Patel <patelnirmal1325@gmail.com>
2022-05-15 19:01:38 -0400

    updated TwitterArchiver class with practice setup code

--- TwitterArchiver/src/main/java/TwitterArchiver.java
+++ TwitterArchiver/src/main/java/TwitterArchiver.java
@@ -1,8 +1,14 @@
-import twitter4j.Status;
-import twitter4j.TwitterFactory;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 
 public class TwitterArchiver {
-    
+    public static void main(String args[]) {
+        try {
+            String latestStatus = "Tweet sent";
+            Twitter twitter = TwitterFactory.getSingleton();
+            Status status = twitter.updateStatus(latestStatus);
+            System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+    }
 }
