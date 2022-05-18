af7e7591c040bf3e05a67a36a91e037391741919
Nirmal Patel <patelnirmal1325@gmail.com>
2022-05-17 21:32:06 -0400

    add user input code

--- TwitterArchiver/src/main/java/TwitterArchiver.java
+++ TwitterArchiver/src/main/java/TwitterArchiver.java
@@ -1,14 +1,27 @@
 import twitter4j.*;
+import java.util.Scanner;
+
 
 public class TwitterArchiver {
     public static void main(String args[]) {
+
         try {
-            String latestStatus = "Tweet sent";
-            Twitter twitter = TwitterFactory.getSingleton();
-            Status status = twitter.updateStatus(latestStatus);
-            System.out.println("Successfully updated the status to [" + status.getText() + "].");
-        } catch (TwitterException e) {
-            e.printStackTrace();
+            //Create a Scanner object called userInput to read user input from keyboard
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Enter the keyword that you want to search with: ");
+
+            //Read userInput string and store in variable keyword
+            String keyword = userInput.nextLine();
+            System.out.println(keyword);
+
+            //Close Scanner object
+            userInput.close();
         }
+        catch (Exception e){
+            //Output error message in case user input is invalid
+            System.out.println("Invalid/Illegal user keyword input");
+
+        }
+        
     }
 }
