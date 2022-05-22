fa6d9fbb0a52559c22f6a83797c02794b2b33904
Nirmal Patel <patelnirmal1325@gmail.com>
2022-05-22 14:18:03 -0400

    added keyword searching code

--- TwitterArchiver/src/main/java/TwitterArchiver.java
+++ TwitterArchiver/src/main/java/TwitterArchiver.java
@@ -1,31 +1,36 @@
 import twitter4j.*;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class TwitterArchiver {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws TwitterException {
+        //Create a Scanner object called userInput to read user input from keyboard
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter the keyword that you want to search with: ");
 
+        //Read userInput string and store in variable keyword
+        String keyword = userInput.nextLine();
+        System.out.println(keyword);
+
+        //Close Scanner object
+        userInput.close();
+
+        //Create TwitterFactory() object called twitter
         try {
-            //Create a Scanner object called userInput to read user input from keyboard
-            Scanner userInput = new Scanner(System.in);
-            System.out.println("Enter the keyword that you want to search with: ");
+            Twitter twitter = new TwitterFactory().getInstance();
+            Query queryKeyword = new Query(keyword);   //query user-input keyword
+            queryKeyword.setCount(2);  //limit search to 10 tweets
+            QueryResult result = twitter.search(queryKeyword);   //search for tweets
 
-            //Read userInput string and store in variable keyword
-            String keyword = userInput.nextLine();
-            System.out.println(keyword);
+            System.out.println("Retrieving tweets, please wait!");  //Output message
+            List<Status> tweets = result.getTweets();   //Store tweets in an ArrayList
+            for (Status tweet : tweets) {
+                System.out.println("@" + tweet.getUser() + "-----" + tweet.getText());
+            }
 
-            //Close Scanner object
-            userInput.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch (Exception e){
-            //Output error message in case user input is invalid
-            System.out.println("Invalid/Illegal user keyword input");
-
-        }
-
-        //Install and connect MongoDB database.
-        //Use keyword as a document key
-        //Connect to twitter account and extract tweets with keyword
-        //Save keywords to MongoDB
-    }
+    };
 }
